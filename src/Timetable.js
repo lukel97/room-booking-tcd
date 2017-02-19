6d3cf46ee0036c922e48397d50125ed3eb223e4b
luke <luke_lau@icloud.com>
2017-02-19 21:46:42 +0000

    Add table to timetable

--- src/Timetable.js
+++ src/Timetable.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class Timetable extends Component {
   
+  constructor(props) {
+    super(props);
+    
+    var times = Array(24);
+    for(var i = 0; i < 24; i++)
+      times[i] = `${i}:00`;
+      
+      this.state = {
+        times: times
+      };
+  }
+  
   render() {
     return (
-      <h1>{this.props.params.facility}</h1>
+      <Container>
+        <h3 className='mt-4'>{this.props.params.facility}</h3>
+        <ListGroup className='mt-4'>
+          {
+            this.state.times.map(x =>
+              <ListGroupItem key={x}>{x}</ListGroupItem> 
+            )
+          }
+        </ListGroup>
+      </Container>
     )
   }
 
