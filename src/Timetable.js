9889b3df3388dcfd6f4349bb3fc28e5fa42ab767
luke <luke_lau@icloud.com>
2017-02-25 01:06:13 +0000

    Add date selector elements to timetable

--- src/Timetable.js
+++ src/Timetable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem } from 'reactstrap';
+import { Container, ListGroup, ListGroupItem, Nav, NavItem, NavLink } from 'reactstrap';
 
 export default class Timetable extends Component {
   
@@ -16,10 +16,54 @@ export default class Timetable extends Component {
   }
   
   render() {
+    let now = new Date();
+    let options = { weekday: 'short', day: 'numeric' };
+    let week = [...Array(14).keys()].map(i => {
+      let date = new Date();
+      date.setDate(now.getDate() + i);
+      return date;
+    });
+
+    const scrollableStyle = {
+      overflowX: 'scroll',
+      whiteSpace: 'nowrap'
+    };
+    
+    const noHorizontalPadding = {
+      paddingLeft: '0',
+      paddingRight: '0'
+    };
+
     return (
       <Container>
         <h3 className='mt-4'>{this.props.params.facility}</h3>
+        
         <ListGroup className='mt-4'>
+          <ListGroupItem style={noHorizontalPadding}>
+            <Nav pills style={scrollableStyle}>
+              <NavItem>
+                <NavLink href="#">Calendar</NavLink>
+              </NavItem>
+              {
+                week.map(date => {
+                  var label;
+                  switch(date.getDate()) {
+                    case now.getDate():
+                      label = "Today";
+                      break;
+                    case now.getDate() + 1:
+                      label = "Tomorrow";
+                      break;
+                    default:
+                      label = date.toLocaleString('en-US', options);
+                  }
+                  return (<NavItem>
+                    <NavLink href="#" active={date.getDate() === now.getDate()}>{label}</NavLink>
+                  </NavItem>);
+                })
+              }
+            </Nav>
+          </ListGroupItem>
           {
             this.state.times.map(x =>
               <ListGroupItem key={x}>{x}</ListGroupItem> 
