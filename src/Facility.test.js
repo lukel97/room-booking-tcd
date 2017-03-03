6bb5cd935e3e96a0acc33b8bb3866212e035e2f2
luke <luke_lau@icloud.com>
2017-03-03 14:41:57 +0000

    Fix tests

--- src/Facility.test.js
+++ src/Facility.test.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import Facilities from './Facility';
+import Facilities, { Facility } from './Facility';
+
+const facilities = [new Facility("Glass Rooms", require("./img/berkeley.jpg")),
+                    new Facility("Berkeley", require("./img/berkeley.jpg")),
+                    new Facility("Hamilton", require("./img/berkeley.jpg")),
+                    new Facility("John Stearne", require("./img/berkeley.jpg"))];
+                    
+const route = { facilities: facilities };
 
 it('shows facilities', () => {
   const div = document.createElement('div');
-  render(<Facilities/>, div);
+  render(<Facilities route={route}/>, div);
   expect(div.querySelectorAll('.card').length > 0).toBe(true);
 });
 
 it('shows the facility name', () => {
   const div = document.createElement('div');
-  render(<Facilities/>, div);
+  render(<Facilities route={route}/>, div);
   expect(div.querySelectorAll('.card-title').length > 0).toBe(true);
   expect(div.querySelectorAll('.card-title')[0].textContent.length > 0).toBe(true);
 });
 
 it('shows the number of free rooms', () => {
   const div = document.createElement('div');
-  render(<Facilities/>, div);
+  render(<Facilities route={route}/>, div);
   expect(div.querySelectorAll('.card-subtitle').length > 0).toBe(true);
   expect(div.querySelectorAll('.card-subtitle')[0].textContent.length > 0).toBe(true);
 });
