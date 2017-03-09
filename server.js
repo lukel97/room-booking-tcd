0ff55ba9a911e0b5b130f25ad05983e668acdb74
luke <luke_lau@icloud.com>
2017-03-09 19:00:40 +0000

    Add endpoint for fetching user bookings from glass rooms

--- server.js
+++ server.js
@@ -14,12 +14,28 @@ function range(begin, end, interval = 1) {
 	return array;
 }
 
+Array.prototype.flatten = function() {
+	return this.reduce((array, x) => array.concat(x), []);
+}
+
+app.get('/facilities/:name/bookings', (req, res) => {
+	
+	//🚨 Dodgy - dont' recommend 🚨
+	let username = req.query.username;
+	let password = req.query.password;
+
+	Promise.all(range(1, 9).map(getMyGlassRoomBookings.bind(null, username, password)))
+			.then(times => times.filter(x => x.bookings.length > 0))
+			.then(times => times.flatten())
+			.then(times => res.end(JSON.stringify(times)), error => res.end("oops"));
+});
+
 app.get('/facilities/:name/', (req, res) => {
 	let date = new Date(req.query.date);
 	//Return the times for all the rooms
 	Promise.all(range(1, 9).map(getGlassRoomTimes.bind(null, date))).then(times => {
 		res.end(JSON.stringify(times));
-	});
+	}, error => res.end("oops"));
 });
 
 const server = app.listen(4000, () => {
@@ -28,6 +44,65 @@ const server = app.listen(4000, () => {
 
   console.log("Listening at http://%s:%s", host, port)
 });
+
+function getMyGlassRoomBookings(username, password, room) {
+	const options = {
+	  protocol: 'https:',
+	  host: 'www.scss.tcd.ie',
+	  path: `https://www.scss.tcd.ie/cgi-bin/webcal/sgmr/sgmr${room}.cancel.pl`,
+	  //args 2 and 3 are the username and password
+	  auth: `${username}:${password}`
+	};
+	
+	return new Promise((resolve, reject) => {
+		https.get(options, function(res) {
+			let rawData = '';
+			res.on('data', (chunk) => rawData += chunk);
+			res.on('end', () => {
+				try {
+					resolve({
+						roomNumber: room,
+						bookings: parseMyGlassRoomBookings(rawData)
+					});
+				} catch(e) {
+					reject(e)
+				}
+			});
+		});
+	})
+	
+}
+
+function parseMyGlassRoomBookings(data) {
+	let $ = cheerio.load(data);
+	
+	let form = $("form[action='/cgi-bin/webcal/sgmr/sgmr3.cancel.pl']");
+	let strings = form.find("input[type='CHECKBOX']").map((i, e) => $(e).val()).get();
+	
+	let bookings = strings.map(s => {
+		let parts = s.split("|");
+		let year = parseInt(parts[0].slice(0, 4));
+		let month = parseInt(parts[0].slice(4, 6)) - 1;	//Months are zero indexed
+		let day = parseInt(parts[0].slice(6, 8));
+		
+		let [from, to] = parts[1].split("-");
+		
+		let hours = parseInt(from.slice(0, 2));
+		
+		let date = new Date();
+		date.setFullYear(year);
+		date.setMonth(month);
+		date.setDate(day);
+		date.setHours(hours);
+		date.setMinutes(0);
+		date.setSeconds(0);
+		date.setMilliseconds(0);
+		
+		return date;
+	});
+	
+	return bookings;
+}
 
 function getGlassRoomTimes(date, room) {
 	let requestData = `Month=${(date.getMonth() + 1)}&Year=${date.getFullYear()}`;
