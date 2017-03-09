d9a00cae1671dba6cc276b8da6706a9f798a3380
Ronan Mac Fhlannchadha <ronan@Ronans-MacBook-Pro.local>
2017-03-09 15:42:10 +0000

    booking reactsrap

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { Card, CardHeader, CardText, CardBlock,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 export default class Bookings extends Component {
 
+	constructor(params) {
+		super(params);
+
+		this.state ={
+			bookings: ["one", "two"]
+      rooms: ["Room 3: Hamilton Glassrooms", "Room 7: Berkely Library"]
+		};
+	}
+
   render() {
+
+  	var cards = [];
+  	for(var i = 0; i < this.state.bookings.length; i++) {
+  		cards.push(<Card>
+        <CardHeader>{this.state.rooms[i]}</CardHeader>
+        <CardBlock>
+          <CardTitle>{this.state.bookings[i]}</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Cancel Booking</Button>
+        </CardBlock>
+      </Card>);
+  	}
+
     return (
-      <h1> bookings </h1>
-    )
+      <div>
+      	{cards}
+      </div>
+    );
   }
 
 }
